e2b4dcb2d2e40796bfd7cabc903db2ed8f32dec1
ronglei <ronglei@lashou-inc.com>
2015-05-05 16:52:53 +0800

    Initial Commit

---
+++ DeepIntoMemory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeepIntoMemory
+//
+//  Created by ronglei on 15/5/5.
+//  Copyright (c) 2015年 ronglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeepIntoMemory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeepIntoMemory
+//
+//  Created by ronglei on 15/5/5.
+//  Copyright (c) 2015年 ronglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
